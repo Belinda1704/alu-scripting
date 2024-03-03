d655e4e72b4bda4a9cd91f818b8a600c20fbf338
Belinda1704 <b.larose@alustudent.com>
2024-03-03 11:48:27 -0800

    Changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import requests
+""" script to obtain subscribers
+    count from a subreddit
+    """
+    from requests import get
 
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    def number_of_subscribers(subreddit):
+            """ function to get subscriber count"""
+                if subreddit and type(subreddit) is str:
+                    subscribers = 0
+                    url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+                    headers = {'user-agent': 'my-app/0.0.1'}
+                    req = get(url, headers=headers)
+                    if req.status_code == 200:
+                        data = req.json()
+                        subscribers = data.get('data', {}).get('subscribers', 0)
+                    return subscribers
